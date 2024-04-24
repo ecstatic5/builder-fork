d7e2ce2de2675a96763605d7147ef4e5637120ed
Isaac <138826243+ecstatic5@users.noreply.github.com>
2024-04-24 15:46:55 -0600

    Added new errors

--- src/Constants/Errors.lua
+++ src/Constants/Errors.lua
@@ -1,4 +1,8 @@
 return {
 	VAR_IS_NIL_ERROR = "Passed variable is nil! Expected to be a '%s'",
 	WRONG_TYPE_ERROR = "Passed variable is a '%s'! Expected to be a '%s'",
+	BIND_WRONG_ERROR = "Unable to bind function '%s': %s",
+	UNABLE_TO_SET_ACTIVE_SELECTION_ERROR = "Unable to set active selection: %s",
+	NIL_PROPERTIES_ERROR = "Properties argument is nil or doesn't exist!",
+	PROPERTIES_ARE_NOT_A_ERROR = "Properties argument is a '%s'! Expected to be a '%s'",
 }
