60a7f5605a2f99580b19bbcae91f22f2fee77eac
iiAngel <90804889+iiAngel@users.noreply.github.com>
2024-04-24 19:13:48 -0600

    Updated main file

--- src/BuilderPlugin.client.lua
+++ src/BuilderPlugin.client.lua
@@ -5,12 +5,10 @@ local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 -- Modules --
 local Initializer = require(script.Parent.Plugin.Initializer)
-Initializer.ApplyDecoratorFolders()
 local Builder = require(script.Parent.Builder).Init()
 -- Plugin --
 local toolbar = plugin:CreateToolbar("Builder")
 local mainButton = toolbar:CreateButton(0, "Enables Builder Plugin", "Builder", "Start Builder")
-
 local mouse = plugin:GetMouse()
 
 if not RunService:IsEdit() and not RunService:IsStudio() then
@@ -25,24 +23,7 @@ local function OnMouseButton1Down()
 	Builder:GetSelectionFromMouse(mouse)
 end
 
-Builder:BindOnSelect(function(object, isDeselected)
-	print(object.Name, isDeselected)
-end)
-
-task.spawn(function()
-	while task.wait(0.01) do
-		if
-			UserInputService:IsKeyDown(Enum.KeyCode.LeftAlt)
-			and UserInputService:IsKeyDown(Enum.KeyCode.Up)
-		then
-			Builder:Resize(0.1)
-		end
-	end
-end)
-
-mouse.Button1Down:Connect(OnMouseButton1Down)
-
-mainButton.Click:Connect(function()
+local function OnPluginButtonClick()
 	if plugin:IsActivated() then
 		plugin:Deactivate()
 		mainButton:SetActive(false)
@@ -50,7 +31,9 @@ mainButton.Click:Connect(function()
 	end
 
 	plugin:Activate(true)
-	Initializer.GetSettings(plugin)
 
 	mainButton:SetActive(true)
-end)
+end
+
+mainButton.Click:Connect(OnPluginButtonClick)
+mouse.Button1Down:Connect(OnMouseButton1Down)
